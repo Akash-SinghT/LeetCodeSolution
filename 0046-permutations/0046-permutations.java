515a70624a56b15aa08b918403791bf3592c5085
Akash Singh <sakashh165@gmail.com>
2025-03-07 11:47:04 +0530

    Time: 1 ms (92.78%), Space: 44.5 MB (64.08%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
+        solve(0,nums,ans,res);
+        return res;
+    }
+    void solve(int start,int nums[],List<Integer> ans,List<List<Integer>> res){
+        if(start>=nums.length){
+            res.add(new ArrayList(ans));
+            return;
+        }
+        for(int i=start;i<nums.length;i++ ){
+            swap(nums,start,i);
+            ans.add(nums[start]);
+            solve(start+1,nums,ans,res);
+            ans.remove(ans.size()-1);
+            swap(nums,i,start);
+        }
+    }
+    void swap(int [] nums,int i,int j){
+       int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+}
