7d265009fa8837419f62ac01c5b2ebc66ab66c0a
Akash Singh <sakashh165@gmail.com>
2025-09-07 18:51:21 +0530

    Time: 2 ms (96.66%), Space: 41.5 MB (94.09%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack <Character> st=new Stack<Character>();
+        for(int i=0;i<s.length();i++){
+            char ch =s.charAt(i);
+            if(ch=='(' || ch=='['|| ch=='{'){
+                st.push(ch);
+            }
+            else{
+                if(!st.isEmpty()){
+                    char top =st.peek();
+                    if(ch==')'&& top=='('){
+                        st.pop();
+                    }
+                    else                     if(ch=='}'&& top=='{'){
+                        st.pop();
+
+                }
+                    else                     if(ch==']'&& top=='['){
+                        st.pop();
+
+            }
+                    else{
+                        return false;
+                    }
+        }
+        else {
+            return false;
+        }
+        
+    }
+}
+        if(st.isEmpty()){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
