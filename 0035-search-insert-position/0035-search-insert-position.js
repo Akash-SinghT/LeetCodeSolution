d51bee88f036c54b4ad359803ee1b790982b4293
Akash Singh <sakashh165@gmail.com>
2025-09-12 00:13:41 +0530

    Time: 0 ms (100%), Space: 54.2 MB (30.36%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let i=0;
+    while(i<nums.length){
+        if(nums[i]>=target){
+            return i;
+        }
+      
+        i++;
+    }
+    return i;
+};
